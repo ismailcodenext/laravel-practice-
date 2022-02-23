d482173c45974b61cc0c752c0b98e6a2d7d0c66f
codenextitD <engr.ismail348@gmail.com>
2022-02-23 14:34:33 +0600

    route-practice

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>This is Contact Page</h1>
+<a href="{{'/'}}">Back to home</a>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
